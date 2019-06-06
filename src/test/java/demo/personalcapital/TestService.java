e7e969a5f024f94247012c9d5f0e29d2d1a77d6b
araghuraman1 <araghuraman1@gmail.com>
2019-06-06 11:30:49 -0700

    Implement tests

---
+++ src/test/java/demo/personalcapital/TestService.java
@@ -0,0 +1,65 @@
+package demo.personalcapital;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Results of these tests can be verified against the CSV file at
+ * http://askebsa.dol.gov/FOIA%20Files/2017/Latest/F_5500_2017_Latest.zip
+ *
+ */
+public class TestService {
+
+    @Test
+    public void testWithPlanNameWildcardAndSponsorState() throws UnirestException {
+
+        HttpResponse<JsonNode> response =
+        Unirest.get("https://drm7p18zhf.execute-api.us-east-2.amazonaws.com/default/search")
+                .queryString("planname","HEITMAN*")
+                .queryString("sponsorstate","KS")
+                .asJson();
+
+        Assertions.assertTrue( Integer.parseInt(String.valueOf(response.getBody().getObject().get("numResults")))==1);
+    }
+
+    @Test
+    public void testWithPlanName() throws UnirestException{
+        HttpResponse<JsonNode> response =
+                Unirest.get("https://drm7p18zhf.execute-api.us-east-2.amazonaws.com/default/search")
+                        .queryString("planname","JVS LTD")
+                        .asJson();
+
+        Assertions.assertTrue( Integer.parseInt(String.valueOf(response.getBody().getObject().get("numResults")))==1);
+
+    }
+
+    @Test
+    public void testWithSponsorState() throws UnirestException{
+        HttpResponse<JsonNode> response =
+                Unirest.get("https://drm7p18zhf.execute-api.us-east-2.amazonaws.com/default/search")
+                        .queryString("planname","KS")
+                        .asJson();
+
+        Assertions.assertTrue( Integer.parseInt(String.valueOf(response.getBody().getObject().get("numResults")))==10);
+
+    }
+
+    @Test
+    public void testWithNoParamsResultLimitedTo10() throws UnirestException{
+        HttpResponse<JsonNode> response = Assertions.assertDoesNotThrow(()->
+                Unirest.get("https://drm7p18zhf.execute-api.us-east-2.amazonaws.com/default/search")
+                        .asJson()
+        );
+
+        Assertions.assertTrue( Integer.parseInt(String.valueOf(response.getBody().getObject().get("numResults")))==10);
+
+    }
+
+
+
+}
